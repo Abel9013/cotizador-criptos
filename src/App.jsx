fdc2a7bb0efe70b8b414f3a0648201edd7264ac9
Abel <90abel.avr@gmail.com>
2022-10-10 01:48:22 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+// Se crea por fuera del componente
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+`
+
+function App() {  
+
+  return (
+   <Heading>Desde App</Heading>
+  )
+}
+
+export default App
