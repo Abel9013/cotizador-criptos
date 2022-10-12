d67c49cb5a9fa411da296c5ad407f522fb860464
Abel <90abel.avr@gmail.com>
2022-10-11 23:08:35 -0300

    add query API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,79 @@
 import styled from '@emotion/styled'
+import { useState, useEffect } from 'react'
+import Formulario from './components/Formulario'
+import ImagenCriptos from './img/imagen-criptos.png'
+
 
 // Se crea por fuera del componente
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width:992px ) { 
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem; 
+  }
+
+`
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
   color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+    }
+`
+const Imagen = styled.img`
+max-width: 400px;
+width: 80%;
+margin: 100px auto 0 auto;
+display: block;
+
 `
 
 function App() {  
-
+      const[monedas, setMonedas] = useState({})
+      const [resultado, setResultado] = useState({})
+      useEffect(() => { 
+        if(Object.keys(monedas).length > 0){
+      
+          const cotizarCripto = async () => {
+            const {moneda, criptomoneda} = monedas
+            const url =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}` 
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+            setResultado(resultado.DISPLAY[criptomoneda][moneda])
+          }
+          cotizarCripto()
+        }
+      },[monedas])
   return (
-   <Heading>Desde App</Heading>
-  )
+   
+    <Contenedor>
+      <Imagen
+          src={ImagenCriptos}
+          alt= "Imagenes Criptomonedas" />
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+        <Formulario
+            setMonedas={setMonedas}
+        />
+
+      </div>        
+      
+    </Contenedor>
+
+   )
 }
 
 export default App
