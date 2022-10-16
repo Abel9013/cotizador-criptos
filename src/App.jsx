35b58d0814f2e21c66f468fdfe2b2a735787e07a
Abel <90abel.avr@gmail.com>
2022-10-16 01:35:38 -0300

    add gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import { useState, useEffect } from 'react'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import ImagenCriptos from './img/imagen-criptos.png'
 
 
@@ -43,17 +44,27 @@ display: block;
 `
 
 function App() {  
+
       const[monedas, setMonedas] = useState({})
       const [resultado, setResultado] = useState({})
+      const [cargando, setCargando] = useState(false)
+  
+  
       useEffect(() => { 
         if(Object.keys(monedas).length > 0){
       
             const cotizarCripto = async () => {
+
+              setCargando(true)
+              setResultado(false)
+
               const {moneda, criptomoneda} = monedas
               const url =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}` 
               const respuesta = await fetch(url)
               const resultado = await respuesta.json()
               setResultado(resultado.DISPLAY[criptomoneda][moneda])
+              
+              setCargando(false)
             }
             cotizarCripto()
         }
@@ -69,6 +80,7 @@ function App() {
         <Formulario
             setMonedas={setMonedas}
         />
+        {cargando && <Spinner/>}
         {resultado.PRICE && <Resultado resultado={resultado } /> }
       </div>        
       
